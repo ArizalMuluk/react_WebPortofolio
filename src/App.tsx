28edca08d6e75f3dce6d61f30611f00d3e63c97d
ArizalMuluk <monsterkikuk@gmail.com>
2025-06-15 08:48:06 +0700

    add analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -45,6 +46,7 @@ function App() {
 
   return (
     <div className="relative">
+      <Analytics />
       <CustomCursor />
       <LoadingScreen />
       <BackgroundAnimation />
